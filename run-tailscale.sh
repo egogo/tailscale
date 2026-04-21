ad7b2b24b825e37dc1fd35043c66eb7d337eb01e
Anton Sokol <anton.sokol@gmail.com>
2026-04-20 21:25:00 -0700

    Modify Tailscale up command and proxy configuration
    Updated Tailscale command to accept routes and modified proxy setup.

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -4,7 +4,7 @@
 PID=$!
 
 ADVERTISE_ROUTES=${ADVERTISE_ROUTES:-10.0.0.0/8}
-until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES"; do
+until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES" --accept-routes; do
   sleep 0.1
 done
 export ALL_PROXY=socks5://localhost:1055/
@@ -12,7 +12,7 @@ tailscale_ip=$(/render/tailscale ip)
 echo "Tailscale is up at IP ${tailscale_ip}"
 
 
-socat TCP-LISTEN:${REV_PORT:-8080},fork,reuseaddr,bind=0.0.0.0 TCP:${REV_HOST}:${REV_PORT:-8080} &
+socat TCP-LISTEN:${REV_PORT:-8080},fork,reuseaddr,bind=0.0.0.0 SOCKS5:localhost:${REV_HOST}:${REV_PORT:-8080},socksport=1055 &
 echo "Proxy to ${REV_HOST}:${REV_PORT:-8080} is up"
 
 
